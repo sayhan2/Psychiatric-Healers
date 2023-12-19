6db4ef66d1e6b075c1f3258c38928a4ff8efd751
Sayhan Azam <108556066+sayhan2@users.noreply.github.com>
2023-12-18 20:39:54 -0800

    roughly configured sticky navbar

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+const port=3002;
 app.use(express.static('public'));
 const homeroute=require('./home');
+app.get('/', (req, res) => {
+    res.render('index'); // This will render views/index.ejs
+  });
 app.use('/', homeroute);
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
 module.exports = app;
